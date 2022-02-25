4196f3a0fb96790272ed132140a94cef927998c5
M Haidar Hanif <git@mhaidarhanif.com>
2022-02-25 15:03:10 +0700

    feat: mantine theme

---
+++ app/data/theme.ts
@@ -0,0 +1,27 @@
+export const themeData = {
+  colorScheme: 'light',
+  colors: {
+    brand: [
+      '#F0BBDD',
+      '#ED9BCF',
+      '#EC7CC3',
+      '#ED5DB8',
+      '#F13EAF',
+      '#F71FA7',
+      '#FF00A1',
+      '#E00890',
+      '#C50E82',
+      '#AD1374',
+    ],
+  },
+  primaryColor: 'red',
+  headings: {
+    sizes: {
+      h1: {
+        fontSize: '4rem',
+        fontWeight: 800,
+        marginBottom: 0,
+      },
+    },
+  },
+}
