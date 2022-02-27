b9c5eec4c63674109d7093ee0b1f0abae939df43
M Haidar Hanif <git@mhaidarhanif.com>
2022-02-28 01:18:56 +0700

    feat: pricing table with responsive layout system

--- app/data/pricing.ts
+++ app/data/pricing.ts
@@ -1,25 +1,96 @@
-export const pricingData = {
+export const pricingFeaturesData = [
+  { icon: 'database', name: 'Database' },
+  { name: 'Database Space' },
+  { name: 'Database Transfer' },
+  { icon: 'auth', name: 'Auth' },
+  { name: 'Users' },
+  { icon: 'storage', name: 'Storage' },
+  { name: 'Storage Space' },
+  { name: 'Storage Transfer' },
+]
+
+export const pricingData = [
+  {
+    name: 'Free',
+    price: { USD: 0, IDR: 0 },
+    info: 'Shared Container',
+    button: {
+      text: 'Sign up for free',
+      to: '/signup',
+    },
+    features: [null, '50 MB', '500 MB', null, 50, null, '250 MB', '500 MB'],
+  },
+  {
+    name: 'Hobby',
+    price: { USD: 5, IDR: 75_000 },
+    info: 'Shared Container',
+    button: {
+      text: 'Sign up now',
+      to: '/signup',
+    },
+    features: [null, '250 MB', '5 GB', null, 500, null, '500 MB', '5 GB'],
+  },
+  {
+    name: 'Pro',
+    price: { USD: 25, IDR: 360_000 },
+    info: 'Dedicated Container',
+    button: {
+      text: 'Sign up now',
+      to: '/signup',
+    },
+    features: [null, '8 GB', '50 GB', null, 50_000, null, '100 GB', '50 GB'],
+  },
+  {
+    name: 'Pay as you go',
+    price: { USD: 99, IDR: 1_499_000 },
+    info: 'Self Hosted / On Premise',
+    button: {
+      text: 'Contact us',
+      to: '/contact',
+    },
+    features: [
+      null,
+      '$0.5 per GB',
+      '$0.09 per GB',
+      null,
+      'Unlimited',
+      null,
+      '$0.05 per GB',
+      '$0.09 per GB',
+    ],
+  },
+]
+
+export const pricingDataLegacy = {
   head: [
     null,
     {
       name: 'Free',
-      price: '0',
+      price: { USD: 0, IDR: 0 },
       info: 'Shared Container',
+      buttonText: 'Sign up for free',
+      buttonTo: '/signup',
     },
     {
       name: 'Hobby',
-      price: '5',
+      price: { USD: 5, IDR: 75_000 },
       info: 'Shared Container',
+      buttonText: 'Sign up now',
+      buttonTo: '/signup',
     },
     {
       name: 'Pro',
-      price: '25',
+      price: { USD: 25, IDR: 360_000 },
       info: 'Dedicated Container',
+      buttonText: 'Sign up now',
+      buttonTo: '/signup',
     },
     {
       name: 'Pay as you go',
-      price: '99',
+      price: { USD: 99, IDR: 1_499_000 },
       info: 'Self Hosted / On Premise',
+      buttonText: 'Contact us',
+      buttonTo: '/contact',
     },
   ],
   body: [
@@ -29,31 +100,31 @@ export const pricingData = {
       rows: [
         {
           name: 'Database Space',
-          features: ['50 MB', '250 MB', '8 GB', '$0.5 Per GB'],
+          features: ['50 MB', '250 MB', '8 GB', '$0.5 per GB'],
         },
         {
           name: 'Transfer Limit',
-          features: ['500 MB', '5 GB', '50 GB', '$0.09 Per GB'],
+          features: ['500 MB', '5 GB', '50 GB', '$0.09 per GB'],
         },
       ],
     },
   ],
   foot: [
     {
-      text: 'Sign up for free',
-      to: '/signup',
+      buttonText: 'Sign up for free',
+      buttonTo: '/signup',
     },
     {
-      text: 'Sign up now',
-      to: '/signup',
+      buttonText: 'Sign up now',
+      buttonTo: '/signup',
     },
     {
-      text: 'Sign up now',
-      to: '/signup',
+      buttonText: 'Sign up now',
+      buttonTo: '/signup',
     },
     {
-      text: 'Contact us',
-      to: '/contact',
+      buttonText: 'Contact us',
+      buttonTo: '/contact',
     },
   ],
 }
--- app/features/pricing/table.tsx
+++ app/features/pricing/table.tsx
@@ -1,10 +1,143 @@
-import { Container } from '@mantine/core'
-import { FunctionComponent } from 'react'
+import {
+  Box,
+  Button,
+  Container,
+  SimpleGrid,
+  Title,
+  Text,
+  Group,
+  useMantineTheme,
+} from '@mantine/core'
+import { FunctionComponent, useState } from 'react'
+import { Link } from 'remix'
+
+import { Icon } from '~/components'
+import { pricingData, pricingFeaturesData } from '~/data'
+
+type TCurrency = 'USD' | 'IDR'
 
 interface PricingTableProps {}
 
-export const PricingTable: FunctionComponent<PricingTableProps> = () => (
-  <Container size="xl">
-    <h1>Table</h1>
-  </Container>
-)
+interface PricingTableResponsiveProps {
+  currency: TCurrency
+}
+
+export const PricingTable: FunctionComponent<PricingTableProps> = () => {
+  // eslint-disable-next-line unused-imports/no-unused-vars
+  const [currency, setCurrency] = useState<TCurrency>('USD')
+
+  const changeCurrency = () => {
+    if (currency === 'USD') {
+      setCurrency('IDR')
+    } else {
+      setCurrency('USD')
+    }
+  }
+
+  return (
+    <Container
+      size="xl"
+      sx={{
+        marginTop: '5rem',
+        marginBottom: '5rem',
+      }}
+    >
+      <Button radius="md" onClick={changeCurrency}>
+        Switch USD/IDR
+      </Button>
+
+      <PricingTableResponsive currency={currency} />
+    </Container>
+  )
+}
+
+export const PricingTableResponsive: FunctionComponent<
+  PricingTableResponsiveProps
+> = ({ currency }) => {
+  const theme = useMantineTheme()
+
+  return (
+    <SimpleGrid
+      spacing={0}
+      cols={5}
+      sx={{
+        marginBottom: '1rem',
+      }}
+    >
+      <Box>
+        <Box sx={{ height: 220 }} />
+        {pricingFeaturesData.map((item) => (
+          <Group>
+            {item.icon ? (
+              <Group
+                sx={{
+                  width: '100%',
+                  padding: '1rem',
+                  background: theme.colors.gray[1],
+                }}
+              >
+                <Icon name={item.icon} />
+                <Text component="span" sx={{ textTransform: 'uppercase' }}>
+                  {item.name}
+                </Text>
+              </Group>
+            ) : (
+              <Box
+                sx={{
+                  width: '100%',
+                  padding: '1rem',
+                  background: theme.white,
+                }}
+              >
+                <Text component="span" sx={{ fontWeight: 'bold' }}>
+                  {item.name}
+                </Text>
+              </Box>
+            )}
+          </Group>
+        ))}
+      </Box>
+
+      {pricingData.map((plan) => (
+        <Group key={plan.name} direction="column" spacing={0}>
+          <Group direction="column" spacing="xs" sx={{ height: 220 }}>
+            <Title order={4}>{plan?.name}</Title>
+            <Group spacing={5}>
+              <Text
+                component="span"
+                sx={{ fontWeight: 'bold', fontSize: '1.5rem' }}
+              >
+                {currency === 'USD' ? '$' : 'Rp '}
+                {plan?.price[currency]}
+              </Text>
+              <span>/project/month</span>
+            </Group>
+            <Text>{plan.info}</Text>
+            <Link to={plan.button.to}>
+              <Button radius="md">{plan.button.text}</Button>
+            </Link>
+          </Group>
+
+          {plan.features.map((item) => (
+            <Box
+              key={item}
+              sx={{
+                width: '100%',
+                padding: '1rem',
+                background: theme.colors.gray[1],
+              }}
+            >
+              {item ? (
+                <span>{item}</span>
+              ) : (
+                <Text component="span" sx={{ opacity: 0 }}>
+                  -
+                </Text>
+              )}
+            </Box>
+          ))}
+        </Group>
+      ))}
+    </SimpleGrid>
+  )
+}
