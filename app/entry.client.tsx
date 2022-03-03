321eda43e177302a041eb310cd9425166cb79466
M Haidar Hanif <git@mhaidarhanif.com>
2022-03-04 02:59:45 +0700

    feat: setup splitbee and react-ga4 in entry.client

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,12 @@
+/* eslint-disable no-undef */
 import { hydrate } from 'react-dom'
 import { RemixBrowser } from 'remix'
 
+import { splitbee, ReactGA } from '~/utils'
+
 hydrate(<RemixBrowser />, document)
+
+if (ENV.NODE_ENV !== 'development') {
+  splitbee.init()
+  ReactGA.initialize(String(ENV.GOOGLE_ANALYTICS_ID))
+}
