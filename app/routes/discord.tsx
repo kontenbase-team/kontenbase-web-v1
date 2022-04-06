2f8a0a34db863ba8f30d2a1af225773e20527dfb
M Haidar Hanif <git@mhaidarhanif.com>
2022-04-06 14:19:55 +0700

    feat: route twitter discord

---
+++ app/routes/discord.tsx
@@ -0,0 +1,10 @@
+import { LoaderFunction, redirect } from 'remix'
+
+import type { SEOHandle } from '@balavishnuvj/remix-seo'
+
+export const handle: SEOHandle = {
+  getSitemapEntries: async () => null,
+}
+
+export const loader: LoaderFunction = async () =>
+  redirect('https://a.kontenbase.com/twitter')
